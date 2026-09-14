79e10f583f7546ac803e238eafc2679e5f8c11e6
bibi <oscar.djuric@gmail.com>
2026-09-14 15:00:17 +0100

    vite fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,16 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
+  vite: {
+    optimizeDeps: {
+      exclude: ['astro'],
+    },
+    ssr: {
+      // Prevents Vite from trying to bundle internal dependencies into temporary SSR files
+      noExternal: true,
+    },
+  },
 });
